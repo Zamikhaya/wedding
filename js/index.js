de7ecedbc66d4c72991d84d27f4a5330370c1c5f
Zamikhaya <zamikhaya.magogotya@weathersa.co.za>
2018-07-08 17:52:05 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
-$(function() {
+/*$(function() {
+  $(".img-w").each(function() {
+    $(this).wrap("<div class='img-c'></div>")
+    let imgSrc = $(this).find("img").attr("src");
+     $(this).css('background-image', 'url(' + imgSrc + ')');
+  }) */
+   
+  $(function() {
   $(".img-w").each(function() {
     $(this).wrap("<div class='img-c'></div>")
     let imgSrc = $(this).find("img").attr("src");
      $(this).css('background-image', 'url(' + imgSrc + ')');
   })
-            
+  
   
   $(".img-c").click(function() {
     let w = $(this).outerWidth()
